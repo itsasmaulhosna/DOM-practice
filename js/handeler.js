9b66b1b964549f2644aa490c1043bc104b8b3212
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-02-14 22:53:01 +0600

    Updated input in js

--- js/handeler.js
+++ js/handeler.js
@@ -6,3 +6,14 @@ document
     // console.log(pageTitlle);
     pageTitlle.innerText = 'Updated Title';
   });
+
+//   input field
+document.getElementById('btn').addEventListener('click', function () {
+  console.log('Button clicked');
+  const inputField = document.getElementById('input');
+  console.log(inputField);
+  const name = inputField.value;
+  console.log(name);
+  const namep = document.getElementById('name-text');
+  namep.innerText = name;
+});
